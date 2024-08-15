017163f67fc9088640135f6add66924f21012283
Yasir Mehmood <129255451+1Yasir@users.noreply.github.com>
2024-08-15 17:56:05 +0500

    complete the expandForm logic

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // We find the greatest and smallest 5-digits numbers  
     const greatestBtn = document.getElementById('greatest');
     const smallestBtn = document.getElementById('smallest');
     const resultDiv = document.getElementById('result');
@@ -12,10 +13,44 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     function largestFiveDigit() {
-        return 99999; 
+        return `<b>${99999}</b>`;
     }
 
     function smallestFiveDigit() {
-        return 10000;
+        return `<b>${10000}</b>`;
+    }
+
+    // Expand the given number
+    const expandInput = document.getElementById("expandInput");
+    const expandBtn = document.getElementById("expandBtn");
+    const expandResult = document.getElementById("expandResult");
+
+    expandBtn.addEventListener("click", () => {
+        const inputValue = Number(expandInput.value.trim());
+        if (inputValue) {
+            expandResult.innerHTML = `<b>Expanded form</b>: ${expandNumber(inputValue)}`;
+        } else {
+            expandResult.innerHTML = `Please enter a valid number.`;
+        }
+    });
+
+    function expandNumber(num) {
+        const numStr = num.toString();
+        const length = numStr.length;
+        let expandedForm = "";
+
+        for (let i = 0; i < length; i++) {
+            const digit = numStr[i];
+            const zeros = length - i - 1;
+            expandedForm += digit + "0".repeat(zeros);
+            
+            if (digit !== "0") {
+                    if (i < length - 1) {
+                    expandedForm += " + ";
+                }
+            }
+        }
+
+        return expandedForm;
     }
 });
